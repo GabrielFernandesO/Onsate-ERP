f5f92a1591519692a4f3ef9d4e99d379d83776d5
joaogfnascimento <joaogfnascimento@hotmail.com>
2024-10-25 18:04:43 -0300

    Adicionado footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import styles from "./page.module.css";
 
@@ -9,6 +10,7 @@ export default function Home() {
       <section className={styles.sectionIMG}>
 
       </section>
+      <Footer />
     </main>
   );
 }
